5a389e96096136964455413da5d90095833e5167
Guillermo <guillermoc93@protonmail.com>
2021-02-15 14:17:50 -0600

    created 16x16 grid using css grid, css vartables, and javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,13 @@
+const container = document.querySelector(".container");
 
-const container = document.getElementById("#container");
+function createGrid(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("grid-cell");
+        container.appendChild(cell);
+    }
+}
 
+createGrid(16, 16);
